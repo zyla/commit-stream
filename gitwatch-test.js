333096e3420ccfda422ae744a8f5bb5840bdce2a
Maciej Bielecki <zyla@prati.pl>
2014-04-30 01:04:08 +0200

    Initial commit

---
+++ gitwatch-test.js
@@ -0,0 +1,14 @@
+var GitWatch = require('./lib/git-watch');
+
+var w = new GitWatch('.');
+
+w.on('open', function() {
+	console.log('current master: ' + w.master);
+});
+
+w.on('newCommits', function(newCommits) {
+	console.log('New commits:');
+	newCommits.forEach(function(commit) {
+		console.log('* ' + commit.sha() + ' ' + commit.message.trim());
+	});
+});
