93b68c4a3a1c91dc5b44393fc257f037153ff525
Maciej Bielecki <zyla@prati.pl>
2014-04-30 01:21:59 +0200

    Little refactor

--- gitwatch-test.js
+++ gitwatch-test.js
@@ -3,7 +3,7 @@ var GitWatch = require('./lib/git-watch');
 var w = new GitWatch('.');
 
 w.on('open', function() {
-	console.log('current master: ' + w.master);
+	console.log('current master: ' + w.refs['refs/heads/master']);
 });
 
 w.on('newCommits', function(newCommits) {
